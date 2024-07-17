229e92fb71b6fb9f67650d458c0d54e2437cb445
Darko Živić <zivic.darko79@gmail.com>
2024-07-17 20:39:40 +0200

    "Updated translations in en.json and hr.json, added ApproachWrapper component with translated titles"

--- src/components/home/ApproachWrapper.jsx
+++ src/components/home/ApproachWrapper.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Approach from './Approach'
+import { useTranslations } from 'next-intl';
 
 const ApproachWrapper = () => {
+  const t = useTranslations('ApproachWrapper');
   return (
-    <div>
+    <div className='py-16'>
+       <p className='mb-4 text-center text-3xl'>
+        <span className='mr-1'>{t('title1')}</span>
+        <span className='mx-1 text-accent'>{t('title2')}</span>
+        <span className='mr-1'>{t('title3')}</span>
+     </p>
         <Approach />
     </div>
   )
