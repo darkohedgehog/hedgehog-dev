65c075893abb122ed0e631154f57e810be03369e
Darko Živić <zivic.darko79@gmail.com>
2024-09-19 20:21:17 +0200

    "Updated blog page and component to fetch data with API token, modified thumbnail URL and image rendering, and added error handling for no content available"

--- src/app/[locale]/blog/[slug]/page.jsx
+++ src/app/[locale]/blog/[slug]/page.jsx
@@ -8,7 +8,11 @@ import { useLocale } from 'next-intl';
 // Funkcija za dohvatanje bloga po slug-u i jeziku
 async function fetchBlogBySlug(slug, locale) {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?locale=${locale}&populate=*`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?filters[slug][$eq]=${slug}&locale=${locale}&populate=*`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_REST_API_KEY}`, // Dodaj API token ovde
+      },
+    });
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
@@ -26,7 +30,7 @@ async function fetchBlogBySlug(slug, locale) {
 
 const BlogDetail = () => {
   const [blog, setBlog] = useState(null);
-  const { slug } = useParams();
+  const { slug } = useParams(); // Uzmi slug iz URL-a
   const locale = useLocale(); // Koristimo useLocale iz next-intl da bismo uzeli jezik
 
   useEffect(() => {
@@ -48,62 +52,67 @@ const BlogDetail = () => {
     return <div>Loading...</div>;
   }
 
-  const thumbnailUrl = blog?.attributes?.thumbnail?.data?.[0]?.attributes?.url;
+  const thumbnailUrl = blog?.thumbnail?.[0]?.formats?.thumbnail?.url;
 
+ 
   return (
     <div className='px-10 md:px-20 py-8'>
       <h1 className='text-[24px] uppercase font-bold mb-16 text-center text-accent'>
-        {blog?.attributes?.title}
+        {blog?.title}
       </h1>
       {thumbnailUrl ? (
         <Image
-          src={`${process.env.NEXT_PUBLIC_BASE_URL}${thumbnailUrl}`} // Lokalni URL za sliku
-          alt={blog.attributes.title}
-          width={800}
-          height={600}
+          src={`${process.env.NEXT_PUBLIC_BASE_URL}${thumbnailUrl}`}
+          alt={blog.title}
+          width={200}
+          height={200}
           priority={false}
-          className='rounded-lg object-cover h-72 w-72 mx-auto shadow-lg'
+          className='rounded-lg object-cover h-40 w-72 mx-auto shadow-lg'
         />
       ) : (
         <div className='w-full h-[300px] bg-gray-200 rounded-lg'></div>
       )}
 
-<div className='mt-12 text-gray dark:text-white flex justify-center items-center flex-col gap-4'>
-  {blog?.attributes?.description?.map((descItem, index) => {
-    if (descItem.type === "paragraph") {
-      // Renderujemo paragraf blok
-      return (
-        <p
-         key={index}>
-          {descItem.children?.map((child, childIndex) => (
-            <span key={childIndex}>{child.text}</span>
-          ))}
-        </p>
-      );
-    }
-    
-    if (descItem.type === "image") {
-      // Renderujemo image blok
-      const imageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${descItem.image.formats.large?.url || descItem.image.url}`;
-      const altText = descItem.alternativeText || "Image";
-      
-      return (
-        <Image
-          key={index}
-          src={imageUrl}
-          alt={altText}
-          height={100}
-          width={200}
-          priority={false}
-          className="my-4 w-1/3 rounded-lg"
-        />
-      );
-    }
+      <div className='mt-12 text-gray dark:text-white flex justify-center items-center flex-col gap-4 bg-card-bg-light dark:bg-card-bg-dark rounded-md shadow-lg shadow-accent dark:shadow-accentDark'>
+        {blog?.description ? (
+          blog.description.map((descItem, index) => {
+            if (descItem.type === "paragraph") {
+              return (
+                <p
+                 className='mx-6 my-5'
+                 key={index}>
+                  {descItem.children?.map((child, childIndex) => (
+                    <span key={childIndex}>{child.text}</span>
+                  ))}
+                </p>
+              );
+            }
 
-    return null; // Ako nije ni tekst ni slika, ne renderujemo ništa
-  })}
-</div>
+            if (descItem.type === "image" && descItem.image) {
+              const imageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${descItem.image.formats?.large?.url || descItem.image.url}`;
+              const altText = descItem.alternativeText || "Image";
 
+              return (
+                <Image
+                  key={index}
+                  src={imageUrl}
+                  alt={altText}
+                  height={100}
+                  width={200}
+                  priority={false}
+                  className="my-4 w-1/3 rounded-lg"
+                />
+              );
+            }
+
+            return null; // Ako nije ni tekst ni slika, ne renderujemo ništa
+          })
+        ) : (
+          <p className='flex items-center justify-center text-red-400'>
+                  No content available
+           </p>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -7,7 +7,11 @@ import { useLocale, useTranslations } from 'next-intl';
 
 async function fetchBlogs(locale) {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?locale=${locale}&populate=*`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?locale=${locale}&populate=*`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_REST_API_KEY}`,
+      },
+    });
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
@@ -18,6 +22,7 @@ async function fetchBlogs(locale) {
     return null;
   }
 }
+
 
 const Blog = () => {
   const t = useTranslations('Blog');
@@ -50,37 +55,43 @@ const Blog = () => {
   return (
     <div className='px-10 md:px-20 mb-10'>
       <h1 className='text-2xl lg:text-3xl uppercase font-bold mb-8 flex items-center justify-center text-accent text-center'>
-      {t('title')}
+        {t('title')}
       </h1>
       <p className='text-xl font-semibold mb-8 flex items-center justify-center text-accent dark:text-white text-clip mx-auto'>
         {t('paragraph')}
       </p>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
         {blogs.map((blog) => {
-          const thumbnailUrl = blog?.attributes?.thumbnail?.data?.[0]?.attributes?.url;
-
+          const thumbnailUrl = blog?.thumbnail?.[0]?.formats?.thumbnail?.url
+  
           return (
             <div key={blog.id} className='border-y-2 border-accent dark:border-accentDark rounded-lg shadow-xl shadow-slate-500 dark:shadow-gray p-5 h-[400px] w-full sm:w-[400px] overflow-auto mx-auto'>
-              <Link href={`/${locale}/blog/${blog.attributes.slug}`} locale={locale} prefetch={false}>
-                {thumbnailUrl ? (
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL}${thumbnailUrl}`} // Lokalni URL za sliku
-                    alt={blog.attributes.title}
-                    width={400}
-                    height={300}
-                    priority
-                    className='rounded-lg object-cover h-60 w-60 mx-auto shadow-md shadow-accent'
-                  />
-                ) : (
-                  <div className='w-full h-[300px] bg-gray-200 rounded-lg'></div>
-                )}
-                <h2 className='text-[18px] font-semibold my-8 text-darkpurple dark:text-accentDark flex items-center justify-center mx-auto text-center'>
-                  {blog.attributes.title}
-                </h2>
-                <p className='text-sm font-semibold my-4 text-gray dark:text-white flex justify-start mx-auto'>
-                {blog.attributes.shortdesc}
+              {blog?.slug ? (
+                <Link href={`/${locale}/blog/${blog.slug}`} locale={locale} prefetch={false}>
+                  {thumbnailUrl ? (
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_BASE_URL}${thumbnailUrl}`}
+                      alt={blog.title}
+                      width={100}
+                      height={100}
+                      priority
+                      className='rounded-lg object-cover h-40 w-72 mx-auto shadow-md shadow-accent'
+                    />
+                  ) : (
+                    <div className='w-full h-[300px] bg-gray-200 rounded-lg'></div>
+                  )}
+                  <h2 className='text-[18px] font-semibold my-8 text-darkpurple flex items-center justify-center mx-auto text-center'>
+                    {blog.title}
+                  </h2>
+                  <p className='text-sm font-semibold my-4 text-gray dark:text-white flex justify-start mx-auto'>
+                    {blog.shortdesc}
+                  </p>
+                </Link>
+              ) : (
+                <p className='flex items-center justify-center text-red-400'>
+                  No blogs available
                 </p>
-              </Link>
+              )}
             </div>
           );
         })}
