21b33fbb46534a2d1fd181fdf6a57754d5b0488d
Darko Živić <zivic.darko79@gmail.com>
2024-09-22 17:10:00 +0200

    "Update favicon.ico and site metadata"

--- src/app/utils/siteMetaData.js
+++ src/app/utils/siteMetaData.js
@@ -3,7 +3,7 @@ const siteMetadata = {
     title: 'Hedgehog Web Developer',
     author: 'Darko Živić',
     headerTitle: 'Hedgehog Web Developer',
-    description: 'Izrada web stranica i aplikacija',
+    description: 'Izrada web stranica i aplikacija. Next.js, React, JavaScript',
     language: 'hr-HR',
     theme: 'system', // system, dark or light
     siteUrl: process.env.SITE_URL || 'http://localhost:3000', // your website URL
@@ -12,8 +12,9 @@ const siteMetadata = {
     email: 'zivic.darko79@gmail.com', 
     github: 'https://github.com/darkohedgehog',
     twitter: 'https://x.com/Zivic_Darko',
-    facebook: 'https://www.facebook.com/Zivicelektro/?locale=hr_HR',
+    facebook: 'https://www.facebook.com/zivic.vu?locale=hr_HR',
     linkedin: 'https://www.linkedin.com/in/darko-%C5%BEivi%C4%87/',
+    instagram: 'https://www.instagram.com/hedgehog_web_developer/',
     locale: 'hr-HR',
   };
   
