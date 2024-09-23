e34a2180a0177175b81e3a8a5fe7a4c85662639f
Darko Živić <zivic.darko79@gmail.com>
2024-09-23 12:11:12 +0200

    "Removed 3D-pin.jsx file and updated AnimatedPin.jsx to import PinContainer from Pin.jsx instead of 3D-pin.jsx"

--- src/components/home/AnimatedPin.jsx
+++ src/components/home/AnimatedPin.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { PinContainer } from "../ui/3d-pin";
+import { PinContainer } from "../ui/Pin";
 import Link from "next/link";
 import Image from "next/image";
 import Img1 from '@/assets/projects/elektro.png';
